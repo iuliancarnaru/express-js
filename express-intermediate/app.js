9d7df7337c57db6371afa05d1fa5704f9d7419d5
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-06-17 22:17:43 +0100

    conditionals in ejs

--- express-intermediate/app.js
+++ express-intermediate/app.js
@@ -9,4 +9,16 @@ app.get("/fallinlovewith/:thing", (req, res) => {
   res.render("love.ejs", { thing });
 });
 
+app.get("/posts", (req, res) => {
+  const posts = [
+    { title: "First post", author: "Iulian" },
+    { title: "First second", author: "Aneta" },
+    { title: "First third", author: "Paul" },
+    { title: "First forth", author: "Marcela" },
+    { title: "First fifth", author: "Izabela" }
+  ];
+
+  res.render("posts.ejs", { posts });
+});
+
 app.listen(PORT, () => `App is listening on port ${PORT}`);
