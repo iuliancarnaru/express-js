3826953d72eea657722a4ac85bf5c9f972893a78
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-06-17 23:13:23 +0100

    adding ddata through POST form

--- express-intermediate/app.js
+++ express-intermediate/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => res.render("home.ejs"));
+// global scope data
+let friends = ["Dan", "Alexandro", "Dani", "Lala"];
+
+// getting data
+app.get("/", (req, res) => res.render("home"));
 
 app.get("/fallinlovewith/:thing", (req, res) => {
   const thing = req.params.thing;
@@ -22,6 +28,17 @@ app.get("/posts", (req, res) => {
   ];
 
   res.render("posts", { posts });
+});
+
+app.get("/friends", (req, res) => {
+  res.render("friends", { friends });
+});
+
+// sending data
+app.post("/addfriend", (req, res) => {
+  let newFriend = req.body.newfriend;
+  friends.push(newFriend);
+  res.redirect("/friends");
 });
 
 app.listen(PORT, () => `App is listening on port ${PORT}`);
