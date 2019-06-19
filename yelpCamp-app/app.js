87b85d9d27e2ccefb10473b309c3da9ea49b66fe
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-06-19 18:53:36 +0100

    yelpCamp app structure

---
+++ yelpCamp-app/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const PORT = 3000;
+const app = express();
+
+// set the templating language
+app.set("view engine", "ejs");
+
+// res.send()
+// res.render()
+
+app.get("/", (req, res) => res.render("index"));
+app.get("/campgrounds", (req, res) => {
+  let campgrounds = [
+    {
+      name: "Salmon Creek",
+      image: "https://farm9.staticflickr.com/8322/7887662552_8667d69960.jpg"
+    },
+    {
+      name: "Granite Hill",
+      image: "https://farm9.staticflickr.com/8020/7538732802_49a42d28d2.jpg"
+    },
+    {
+      name: "Lake District",
+      image: "https://farm5.staticflickr.com/4117/4741325076_2d2b5d70dc.jpg"
+    }
+  ];
+
+  res.render("campgrounds");
+});
+
+app.listen(PORT, () =>
+  console.log(` --> YelpCamp server is listening on port: ${PORT}`)
+);
