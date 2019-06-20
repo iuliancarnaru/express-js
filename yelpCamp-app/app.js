2d06a708433e2dd67c5c257b8149f81f1a5d4d6b
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-06-20 22:31:07 +0100

    adding mongoose

--- yelpCamp-app/app.js
+++ yelpCamp-app/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
+// const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
 
 const DATABASE_URL =
   "mongodb+srv://icarnaru:cluster__@yelpcamp-zfvgz.mongodb.net/test?retryWrites=true&w=majority";
@@ -14,10 +15,17 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-  const client = new MongoClient(DATABASE_URL, { useNewUrlParser: true });
-  client.connect(err => {
-    collection = client.db(DATABASE_NAME).collection("users");
-    console.log(` --> Connected to: ${DATABASE_NAME}`);
+  // const client = new MongoClient(DATABASE_URL, { useNewUrlParser: true });
+  // client.connect(err => {
+  //   collection = client.db(DATABASE_NAME).collection("users");
+  //   console.log(` --> Connected with MongoDB Atlas to: ${DATABASE_NAME}`);
+  // });
+
+  mongoose.connect(DATABASE_URL, { useNewUrlParser: true });
+  let db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", function() {
+    console.log(` --> Connected with Mongoose to: ${DATABASE_NAME}`);
   });
   console.log(` --> YelpCamp server is listening on port: ${PORT}`);
 });
